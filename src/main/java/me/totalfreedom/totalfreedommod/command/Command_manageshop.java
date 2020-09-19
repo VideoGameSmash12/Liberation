ac3d0fca88c071dcaa2c9e4b1a97803475754dc5
Seth <13510767+ZeroEpoch1969@users.noreply.github.com>
2020-09-18 18:04:21 -0700

    Allow executives to manage the shop

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_manageshop.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_manageshop.java
@@ -1,9 +1,9 @@
 package me.totalfreedom.totalfreedommod.command;
 
-import me.totalfreedom.totalfreedommod.config.ConfigEntry;
 import me.totalfreedom.totalfreedommod.player.PlayerData;
 import me.totalfreedom.totalfreedommod.rank.Rank;
 import me.totalfreedom.totalfreedommod.shop.ShopItem;
+import me.totalfreedom.totalfreedommod.util.FUtil;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -18,7 +18,7 @@ public class Command_manageshop extends FreedomCommand
     public boolean run(final CommandSender sender, final Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
 
-        if (!ConfigEntry.SERVER_OWNERS.getStringList().contains(sender.getName()))
+        if (!FUtil.isExecutive(sender.getName()))
         {
             return noPerms();
         }
