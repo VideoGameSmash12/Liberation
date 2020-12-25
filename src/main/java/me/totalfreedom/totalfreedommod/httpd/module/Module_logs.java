361aa4ee04d4b99c59cb7fb8bd75595ef4178779
Paldiu <pawereus@gmail.com>
2020-12-25 15:20:34 -0500

    Bugfix on Commit #5c0f77c
    A mismatched argument count during constructor initialization in Module_logs.class was fixed in this patch.

--- src/main/java/me/totalfreedom/totalfreedommod/httpd/module/Module_logs.java
+++ src/main/java/me/totalfreedom/totalfreedommod/httpd/module/Module_logs.java
@@ -11,7 +11,7 @@ public class Module_logs extends Module_file
 
     public Module_logs(TotalFreedomMod plugin, NanoHTTPD.HTTPSession session)
     {
-        super(plugin, session);
+        super(session);
     }
 
     @Override
