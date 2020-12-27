0c60cbc739dbd82826440f834fedb8b0249c8369
Elmon11 <elmon11bussiness@gmail.com>
2020-12-28 00:18:05 +0100

    Update Command_gcmd.java

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_gcmd.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_gcmd.java
@@ -3,6 +3,7 @@ package me.totalfreedom.totalfreedommod.command;
 import me.totalfreedom.totalfreedommod.rank.Rank;
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.command.Command;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
