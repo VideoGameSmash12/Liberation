TotalFreedom/TotalFreedomMod

commit 76bb2d08acb9a56a845377e2106a8f7e2d8a0750

Split gcmd sub cmds out n other stuff

src/main/java/me/totalfreedom/totalfreedommod/TotalFreedomMod.java
@@ -181,33 +181,20 @@ public class TotalFreedomMod extends JavaPlugin
         FUtil.deleteFolder(new File("./_deleteme"));
 
         fsh = new FreedomServiceHandler();
+
+        config = new MainConfig();
+        config.load();
+
         cl = new CommandLoader();
-
-        Reflections commandDir = new Reflections("me.totalfreedom.totalfreedommod.command");
-
-        Set<Class<? extends FreedomCommand>> commandClasses = commandDir.getSubTypesOf(FreedomCommand.class);
-
-        for (Class<? extends FreedomCommand> commandClass : commandClasses)
-        {
-            try
-            {
-                cl.add(commandClass.newInstance());
-            }
-            catch (InstantiationException | IllegalAccessException | ExceptionInInitializerError ex)
-            {
-                FLog.warning("Failed to register command: /" + commandClass.getSimpleName().replace("Command_" , ""));
-            }
-        }
+        cl.loadCommands();
 
         BackupManager backups = new BackupManager();
-        backups.createBackups(TotalFreedomMod.CONFIG_FILENAME, true);
-        backups.createBackups(PermbanList.CONFIG_FILENAME);
-        backups.createBackups(PermissionConfig.PERMISSIONS_FILENAME, true);
-        backups.createBackups(PunishmentList.CONFIG_FILENAME);
-        backups.createBackups("database.db");
+        backups.createAllBackups();
 
-        config = new MainConfig(this);
-        config.load();
+        if (FUtil.inDeveloperMode())
+        {
+            FLog.debug("Developer mode enabled.");
+        }
 
         permissions = new PermissionConfig(this);
         permissions.load();

pom.xml
@@ -240,8 +240,15 @@
         <dependency>
             <groupId>com.github.speedxx</groupId>
             <artifactId>TFGuilds</artifactId>
-            <version>master-SNAPSHOT</version>
+            <version>master</version>
         </dependency>
+
+        <dependency>
+            <groupId>org.apache.maven.plugins</groupId>
+            <artifactId>maven-checkstyle-plugin</artifactId>
+            <version>3.1.1</version>
+        </dependency>
+
 
     </dependencies>
 
@@ -418,7 +425,7 @@
         <plugins>
             <plugin>
                 <groupId>org.apache.maven.plugins</groupId>
-                <version>2.17</version>
+                <version>3.1.1</version>
                 <artifactId>maven-checkstyle-plugin</artifactId>
                 <configuration>
                     <configLocation>checkstyle.xml</configLocation>

src/main/java/me/totalfreedom/totalfreedommod/BackupManager.java
@@ -2,7 +2,10 @@ package me.totalfreedom.totalfreedommod;
 
 import com.sk89q.worldedit.bukkit.BukkitConfiguration;
 import java.io.File;
+import me.totalfreedom.totalfreedommod.banning.PermbanList;
 import me.totalfreedom.totalfreedommod.config.YamlConfig;
+import me.totalfreedom.totalfreedommod.permissions.PermissionConfig;
+import me.totalfreedom.totalfreedommod.punishments.PunishmentList;
 import me.totalfreedom.totalfreedommod.util.FLog;
 import me.totalfreedom.totalfreedommod.util.FUtil;
 import org.bukkit.util.FileUtil;
@@ -23,6 +26,15 @@ public class BackupManager extends FreedomService
     public void createBackups(String file)
     {
         createBackups(file, false);
+    }
+
+    public void createAllBackups()
+    {
+        createBackups(TotalFreedomMod.CONFIG_FILENAME, true);
+        createBackups(PermbanList.CONFIG_FILENAME);
+        createBackups(PermissionConfig.PERMISSIONS_FILENAME, true);
+        createBackups(PunishmentList.CONFIG_FILENAME);
+        createBackups("database.db");
     }
 
     public void createBackups(String file, boolean onlyWeekly)

src/main/java/me/totalfreedom/totalfreedommod/banning/Ban.java
@@ -166,7 +166,20 @@ public class Ban implements IConfig
     {
         final StringBuilder message = new StringBuilder(ChatColor.GOLD + "You");
 
-        message.append(!hasUsername() ? "r IP address is" : " are").append(" temporarily banned from this server.");
+        if (!hasUsername())
+        {
+            message.append("r IP address is");
+        }
+        else if (!hasIps())
+        {
+            message.append("r username is");
+        }
+        else
+        {
+            message.append(" are");
+        }
+
+        message.append(" temporarily banned from this server.");
         message.append("\nAppeal at ").append(ChatColor.BLUE)
                 .append(ConfigEntry.SERVER_BAN_URL.getString());
 

src/main/java/me/totalfreedom/totalfreedommod/bridge/TFGuildsBridge.java
@@ -3,14 +3,13 @@ package me.totalfreedom.totalfreedommod.bridge;
 import me.totalfreedom.tfguilds.Common;
 import me.totalfreedom.tfguilds.TFGuilds;
 import me.totalfreedom.totalfreedommod.FreedomService;
-import me.totalfreedom.totalfreedommod.util.FLog;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public class TFGuildsBridge extends FreedomService
 {
 
-    private TFGuilds tfGuildsPlugin = null;
+    public boolean enabled = false;
 
     @Override
     public void onStart()
@@ -22,31 +21,39 @@ public class TFGuildsBridge extends FreedomService
     {
     }
 
-    public TFGuilds getTfGuildsPlugin()
+    public boolean isTFGuildsEnabled()
     {
-        if (tfGuildsPlugin == null)
+        if (enabled)
         {
-            try
+            return true;
+        }
+
+        try
+        {
+            final Plugin tfGuilds = server.getPluginManager().getPlugin("TFGuilds");
+            if (tfGuilds != null && tfGuilds.isEnabled())
             {
-                final Plugin tfGuilds = server.getPluginManager().getPlugin("TFGuilds");
-                if (tfGuilds != null)
+                if (tfGuilds instanceof TFGuilds)
                 {
-                    if (tfGuilds instanceof TFGuilds)
-                    {
-                        tfGuildsPlugin = (TFGuilds)tfGuilds;
-                    }
+                    enabled = true;
+                    return true;
                 }
             }
-            catch (Exception ex)
-            {
-                FLog.severe(ex);
-            }
         }
-        return tfGuildsPlugin;
+        catch (NoClassDefFoundError ex)
+        {
+            return false;
+        }
+
+        return false;
     }
 
     public boolean inGuildChat(Player player)
     {
+        if (!isTFGuildsEnabled())
+        {
+            return false;
+        }
         return Common.IN_GUILD_CHAT.contains(player);
     }
 }

src/main/java/me/totalfreedom/totalfreedommod/command/CommandLoader.java
@@ -3,8 +3,11 @@ package me.totalfreedom.totalfreedommod.command;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import lombok.Getter;
 import me.totalfreedom.totalfreedommod.FreedomService;
+import me.totalfreedom.totalfreedommod.util.FLog;
+import org.reflections.Reflections;
 
 public class CommandLoader extends FreedomService
 {
@@ -50,6 +53,26 @@ public class CommandLoader extends FreedomService
                 return true;
         }
         return false;
+    }
+
+    public void loadCommands()
+    {
+        Reflections commandDir = new Reflections("me.totalfreedom.totalfreedommod.command");
+
+        Set<Class<? extends FreedomCommand>> commandClasses = commandDir.getSubTypesOf(FreedomCommand.class);
+
+        for (Class<? extends FreedomCommand> commandClass : commandClasses)
+        {
+            try
+            {
+                FLog.debug("Loading command class " + commandClass.getSimpleName());
+                add(commandClass.newInstance());
+            }
+            catch (InstantiationException | IllegalAccessException | ExceptionInInitializerError ex)
+            {
+                FLog.warning("Failed to register command: /" + commandClass.getSimpleName().replace("Command_" , ""));
+            }
+        }
     }
 
     public int getCommandAmount()

src/main/java/me/totalfreedom/totalfreedommod/command/Command_banip.java
@@ -1,0 +1,82 @@
+package me.totalfreedom.totalfreedommod.command;
+
+import me.totalfreedom.totalfreedommod.banning.Ban;
+import me.totalfreedom.totalfreedommod.rank.Rank;
+import me.totalfreedom.totalfreedommod.util.FLog;
+import me.totalfreedom.totalfreedommod.util.FUtil;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH, blockHostConsole = true)
+@CommandParameters(description = "Bans the specified ip.", usage = "/<command> <ip> [reason] [-q]")
+public class Command_banip extends FreedomCommand
+{
+
+    @Override
+    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length == 0)
+        {
+            return false;
+        }
+
+        boolean silent = false;
+
+        String reason = null;
+        
+        String ip = args[0];
+
+        if (!FUtil.isValidIPv4(ip))
+        {
+            msg(ip + " is not a valid IP address", ChatColor.RED);
+            return true;
+        }
+
+        if (plugin.bm.getByIp(ip) != null)
+        {
+            msg("The IP " + ip + " is already banned", ChatColor.RED);
+            return true;
+        }
+
+        if (args[args.length - 1].equalsIgnoreCase("-q"))
+        {
+            silent = true;
+
+            if (args.length >= 2)
+            {
+                reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length - 1), " ");
+            }
+        }
+        else if (args.length > 1)
+        {
+            reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+        }
+
+        // Ban player
+        Ban ban = Ban.forPlayerIp(ip, sender, null, reason);
+        plugin.bm.addBan(ban);
+
+        // Kick player and handle others on IP
+        for (Player player : server.getOnlinePlayers())
+        {
+            if (FUtil.getIp(player).equals(ip))
+            {
+                player.kickPlayer(ban.bakeKickMessage());
+            }
+
+            if (!silent)
+            {
+                // Broadcast
+                FLog.info(ChatColor.RED + sender.getName() + " - Banned the IP " + ip);
+                String message = ChatColor.RED + sender.getName() + " - Banned " + (plugin.al.isAdmin(player) ? "the IP " + ip : "an IP");
+                player.sendMessage(message);
+            }
+        }
+
+        return true;
+    }
+}

src/main/java/me/totalfreedom/totalfreedommod/command/Command_banname.java
@@ -1,0 +1,68 @@
+package me.totalfreedom.totalfreedommod.command;
+
+import me.totalfreedom.totalfreedommod.banning.Ban;
+import me.totalfreedom.totalfreedommod.rank.Rank;
+import me.totalfreedom.totalfreedommod.util.FUtil;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH, blockHostConsole = true)
+@CommandParameters(description = "Bans the specified name.", usage = "/<command> <name> [reason] [-q]")
+public class Command_banname extends FreedomCommand
+{
+
+    @Override
+    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length == 0)
+        {
+            return false;
+        }
+
+        boolean silent = false;
+
+        String reason = null;
+        
+        String name = args[0];;
+
+        if (plugin.bm.getByUsername(name) != null)
+        {
+            msg("The name " + name + " is already banned", ChatColor.RED);
+            return true;
+        }
+
+        if (args[args.length - 1].equalsIgnoreCase("-q"))
+        {
+            silent = true;
+
+            if (args.length >= 2)
+            {
+                reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length - 1), " ");
+            }
+        }
+        else if (args.length > 1)
+        {
+            reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+        }
+
+        // Ban player
+        Ban ban = Ban.forPlayerName(name, sender, null, reason);
+        plugin.bm.addBan(ban);
+
+        if (!silent)
+        {
+            FUtil.adminAction(sender.getName(), "Banned the name " + name, true);
+        }
+
+        Player player = getPlayer(name);
+        if (player != null)
+        {
+            player.kickPlayer(ban.bakeKickMessage());
+        }
+        return true;
+    }
+}

VideoGameSmash12/Liberation

commit e99aaa2eb4665aac0686d029774d604dc3118279

Fixes FS-37
Fixes an oversight introduced in https://github.com/AtlasMediaGroup/TotalFreedomMod/commit/76bb2d08acb9a56a845377e2106a8f7e2d8a0750 in which the configuration is loaded twice.

src/main/java/me/totalfreedom/totalfreedommod/TotalFreedomMod.java
@@ -187,7 +187,6 @@ public class TotalFreedomMod extends JavaPlugin
         fsh = new FreedomServiceHandler();
 
         config = new MainConfig();
-        config.load();
 
         if (FUtil.inDeveloperMode())
         {