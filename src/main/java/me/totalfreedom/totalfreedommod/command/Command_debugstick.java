f9fa9cdf2cfff395aef3802ffb22478ec246b777
Video <videogamesm12@gmail.com>
2022-07-31 11:48:28 -0600

    Updates /debugstick to use Adventure

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_debugstick.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_debugstick.java
@@ -1,9 +1,10 @@
 package me.totalfreedom.totalfreedommod.command;
 
 import java.util.Arrays;
-import java.util.List;
 import me.totalfreedom.totalfreedommod.rank.Rank;
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -15,20 +16,20 @@ import org.bukkit.inventory.meta.ItemMeta;
 @CommandParameters(description = "Get a stick of happiness.", usage = "/<command>")
 public class Command_debugstick extends FreedomCommand
 {
-
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
         ItemStack itemStack = new ItemStack(Material.DEBUG_STICK);
         ItemMeta itemMeta = itemStack.getItemMeta();
-        assert itemMeta != null;
-        itemMeta.setDisplayName(ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "Stick of Happiness");
-        List<String> lore = Arrays.asList(
-                ChatColor.RED + "This is the most powerful stick in the game.",
-                ChatColor.DARK_BLUE + "You can left click to select what you want to change.",
-                ChatColor.DARK_GREEN + "And then you can right click to change it!",
-                ChatColor.DARK_PURPLE + "Isn't technology amazing?");
-        itemMeta.setLore(lore);
+
+        itemMeta.displayName(Component.text("Stick of Happiness", TextColor.color(0xFFAA00)).decorate(TextDecoration.BOLD));
+        itemMeta.lore(Arrays.asList(
+                Component.text("This is the most powerful stick in the game.", TextColor.color(0xFF5555)),
+                Component.text("You can left click to select what you want to change.", TextColor.color(0x0000AA)),
+                Component.text("And then you can right click to change it!", TextColor.color(0x00AA00)),
+                Component.text("Isn't technology amazing?", TextColor.color(0xAA00AA))
+        ));
+
         itemStack.setItemMeta(itemMeta);
         playerSender.getInventory().addItem(itemStack);
         return true;
