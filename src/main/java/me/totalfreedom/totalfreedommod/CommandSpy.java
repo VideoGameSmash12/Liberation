f5e354787078e9f4b34b63486e7b9fc4aa5e8090
Video <videogamesm12@gmail.com>
2022-08-02 15:58:33 -0600

    Better readability for CommandSpy

--- src/main/java/me/totalfreedom/totalfreedommod/CommandSpy.java
+++ src/main/java/me/totalfreedom/totalfreedommod/CommandSpy.java
@@ -4,6 +4,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -25,8 +26,10 @@ public class CommandSpy extends FreedomService
                 Component.text(": "),
                 Component.text(event.getMessage())
         );
-        component.color(TextColor.color(0xAAAAAA));
+        component.color(NamedTextColor.GRAY);
 
-        server.getOnlinePlayers().stream().filter(player -> plugin.al.isAdmin(player) && plugin.al.getAdmin(player).getCommandSpy() && player != from).forEach(player -> player.sendMessage(component));
+        server.getOnlinePlayers().stream().filter(player -> plugin.al.isAdmin(player)
+                && plugin.al.getAdmin(player).getCommandSpy()
+                && player != from).forEach(player -> player.sendMessage(component));
     }
 }
